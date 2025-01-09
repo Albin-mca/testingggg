f8c77741655ff93aa9ea1789b573854bfa08039e
Albin George <94976030+Albin-mca@users.noreply.github.com>
2025-01-09 15:17:55 +0400

    Create sizeof.c

---
+++ sizeof.c
@@ -0,0 +1,9 @@
+int myInt;
+float myFloat;
+double myDouble;
+char myChar;
+
+printf("%lu\n", sizeof(myInt));
+printf("%lu\n", sizeof(myFloat));
+printf("%lu\n", sizeof(myDouble));
+printf("%lu\n", sizeof(myChar));
